9ff9d87625d38deefe461bf7b0a75554390a79dd
Wambugu Jon <mbariwambugu@gmail.com>
2022-06-01 18:42:48 +0300

    12. Fizz Buzz.

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-def print_last_digit(number):
-    if number < 0:
-        number = -number
-    last = number % 10
-    print(last, end='')
-    return last
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 15 == 0:
+            print("FizzBuzz", end=" ")
+        elif i % 3 == 0:
+            print("Fizz", end=" ")
+        elif i % 5 == 0:
+            print("Buzz", end=" ")
+        else:
+            print(i, end=" ")
