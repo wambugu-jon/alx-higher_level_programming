5fe7536abad71dc0d546499f73f96308425acab1
Wambugu Jon <mbariwambugu@gmail.com>
2022-06-03 16:51:46 +0300

    3. Infinite addition.

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+import sys
+
 if __name__ == "__main__":
-    import sys
-    result = 0
-    for i in range(1, len(sys.argv)):
-        result += int(sys.argv[i])
-    print("{}".format(result))
+    sum = 0
+    for i in range(len(sys.argv) - 1):
+        sum = sum + int(sys.argv[i+1])
+    print("{}".format(sum))
