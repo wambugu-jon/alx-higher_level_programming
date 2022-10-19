5f182af5e7af6a9fc166a7d726adff6934115431
Wambugu Jon <38904461+wambugu-jon@users.noreply.github.com>
2022-10-19 10:35:55 +0300

    Loripsum

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,15 +1,5 @@
 #!/usr/bin/node
-
-const request = require('request');
 const fs = require('fs');
+const request = require('request');
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
 
-request(process.argv[2], function (error, response, body) {
-  if (error) {
-    console.error(error);
-  }
-  try {
-    fs.writeFile(process.argv[3], body, 'utf8', function (err, result) { if (err) console.log(err); });
-  } catch (err) {
-    console.log(err);
-  }
-});
